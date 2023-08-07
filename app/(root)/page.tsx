8fd12fcb07768f83bc987ce69d80c0ec3120f8b9
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-08-07 05:56:39 +0100

    update: completed the home page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 //app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
+    {/* <SignInButton></SignInButton> */}
       <h1 className="head-text text-left">Home</h1>
     </>
   )
